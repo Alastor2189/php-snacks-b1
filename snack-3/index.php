75d3de43f221ba7786a3ce2fae94c04831422719
Alastor2189 <g.lucafarci89@gmail.com>
2022-06-08 19:30:56 +0200

    completamento snack 3

--- snack-3/index.php
+++ snack-3/index.php
@@ -1,2 +1,84 @@
 <!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
 Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $posts = [
+
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
+    ?>
+
+    <ul>
+        <?php
+        for ($i = 0; $i < count(array_keys($posts)); $i++) {
+            $date = array_keys($posts)[$i]; ?>
+            <li>
+                <h3> Post pubblicato in data <?php echo $date; ?></h3>
+                <?php
+                for ($j = 0; $j < count($posts[$date]); $j++) {
+                    $public_post = $posts[$date][$j];
+                ?>
+                    <ul>
+                        <li>
+                            <h4> Titolo: <?php echo $public_post['title']; ?></h4>
+                            <p> Autore: <?php echo $public_post['author']; ?></p>
+                            <p> Testo: <?php echo $public_post['text']; ?></p>
+                        </li>
+                    </ul>
+                <?php } ?>
+            </li>
+        <?php } ?>
+    </ul>
+
+
+</body>
+
+</html>
